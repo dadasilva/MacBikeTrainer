9fcb02934989e3a131b5b7dd902bae985ad1b464
David DaSilva <daviddasilvajr@me.com>
2020-09-19 20:59:09 -0400

    Initial Commit

---
+++ unbearable/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  unbearable
+//
+//  Created by David Da Silva on 9/19/20.
+//  Copyright © 2020 David Da Silva. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ unbearable/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  unbearable
+//
+//  Created by David Da Silva on 9/19/20.
+//  Copyright © 2020 David Da Silva. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
